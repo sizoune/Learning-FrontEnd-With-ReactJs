5ce3c670c38412564d45970fa158e6f105390aa6
muhammad wildan iskandar <muhammadwildaniskandar@gmail.com>
2023-10-26 12:23:39 +0800

    note app body finish

---
+++ src/components/NoteAppBody.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import autoBind from 'react-auto-bind';
+import { getInitialData } from '../utils/index.js';
+import NoteList from './NoteList.jsx';
+
+class NoteAppBody extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: getInitialData(),
+    };
+
+    autoBind(this);
+  }
+
+  onDeleteHandler(id) {
+    const notes = this.state.notes.filter((note) => note.id !== id);
+    this.setState({ notes });
+  }
+
+  onMoveHandler(id) {
+    this.setState((prevState) => {
+      const selectedNote = prevState.notes.find((item) => item.id === id);
+      selectedNote.archived = !selectedNote.archived;
+      return {
+        notes: prevState.notes,
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className="note-app__body">
+        <h2>Catatan Aktif</h2>
+        {this.state.notes.filter((note) => !note.archived).length > 0
+          ? (
+            <NoteList
+              notes={this.state.notes.filter((note) => !note.archived)}
+              onDelete={this.onDeleteHandler}
+              onMove={this.onMoveHandler}
+            />
+          )
+          : <p>Tidak Ada Catatan!</p>}
+        <h2>Arsip</h2>
+        {this.state.notes.filter((note) => note.archived).length > 0
+          ? (
+            <NoteList
+              notes={this.state.notes.filter((note) => note.archived)}
+              onDelete={this.onDeleteHandler}
+              onMove={this.onMoveHandler}
+            />
+          )
+          : <p>Tidak Ada Catatan!</p>}
+      </div>
+    );
+  }
+}
+
+export default NoteAppBody;
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 // import style
 import './styles/style.css';
+import NoteAppBody from './components/NoteAppBody.jsx';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<h1>Hello, World!</h1>);
+root.render(<NoteAppBody />);
